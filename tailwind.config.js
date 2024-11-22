51e16f195d5078de53acb03f0e1dbd9e54c8a591
Hungww <luuchanhung123@gmail.com>
2024-11-23 03:48:51 +0700

    quick add navbar, merge directly

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        // Override md to use a custom breakpoint (e.g., 900px)
+        md: "1063px",
+        // Optionally override other breakpoints or add new ones
+        lg: "1100px",
+        miic: "590px"
+      },
+
+      colors: {
+        primary: "#F3544B",
+        background: "#FBDDDC",
+      },
+      borderWidth: {
+        3: '3px', // Defines border-3 as 3px width
+      },
+      fontFamily: {
+        poppins: ["Poppins"],
+        suez: ["Suez One"],
+      },
+    },
   },
   plugins: [],
-}
+};
