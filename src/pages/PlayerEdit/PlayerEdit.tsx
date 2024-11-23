6aebe0a0976a3aab77ea6c8a2d7f529a3afd6bfd
Pham Anh Kiet <kapham231@gmail.com>
2024-11-23 17:07:24 +0700

    update header

--- src/pages/PlayerEdit/PlayerEdit.tsx
+++ src/pages/PlayerEdit/PlayerEdit.tsx
@@ -1,58 +1,46 @@
-import UserInfo from "../UserDetail/UserInfo/UserInfo";
-import UserPayment from "../UserDetail/UserPayment/UserPayment";
-import UserContentHeader from "../UserDetail/UserContentHeader/UserContentHeader";
-import UserGameItem from "../UserDetail/UserGameItem/UserGameItem";
-import UserImg from "../UserDetail/UserImg/UserImg";
 import PlayerFreeTime from "./PlayerFreeTime/playerfreetime";
-import "./PlayerEdit.css";
+import avt from '../../assets/img/anhmau.jpg';
 import PlayerRentalPrice from "./PlayerRentalPrice/PlayerRentalPrice";
 import PlayerBookingList from "./PlayerBookingList/PlayerBookingList";
+import PlayerHeader from "../../components/player-header";
+
+import "./PlayerEdit.css";
 
 const PlayerEdit = () => {
     return (
-        <div className="user_detail bg-[#E1E1E1]">
-            <div className="left_sidebar">
-                <UserInfo />
-            </div>
+        <div>
+            <PlayerHeader id={3} />
 
-            <div className="content">
-                <div className="content_header">
-                    <div className="content_header--top">
-                        <h3 className="user_name">Hươnggggg</h3>
-                        <button className="base_btn follow_btn">Theo dõi</button>
-                    </div>
-
-                    <div className="content_header--bottom">
-                        <UserContentHeader title="Số người theo dõi" content="518" unit="người" />
-                        <UserContentHeader title="Đã được thuê" content="600" unit="giờ" />
-                        <UserContentHeader title="Tỷ lệ hoàn thành" content="84.17" unit="%" />
-                    </div>
-                </div>
-
-
-                <div className="content_base content_game">
-                    <PlayerRentalPrice />
-
-
-                    <h3>Thời gian rãnh</h3>
-                    <div className="game_list">
-                        <PlayerFreeTime id={1} />
-                        <PlayerFreeTime id={2} />
-                        <PlayerFreeTime id={3} />
-                        <PlayerFreeTime id={4} />
+            <div className="player-edit-container">
+                {/* Phần trên: Thông tin người chơi */}
+                <div className="player-top-section">
+                    <h3 className="player-title">THÔNG TIN</h3>
+                    <div className="player-top-content">
+                        <div className="player-avatar-container">
+                            <img src={avt} alt="avatar" className="player-avatar" />
+                        </div>
+                        <div className="player-info-container">
+                            <h2 className="player-name">Dũnggggggg Kaka</h2>
+                            <PlayerRentalPrice />
+                            <h3 className="section-title">Thời gian rãnh</h3>
+                            <div className="game-list">
+                                <PlayerFreeTime id={1} />
+                                <PlayerFreeTime id={2} />
+                                <PlayerFreeTime id={3} />
+                                <PlayerFreeTime id={4} />
+                            </div>
+                        </div>
                     </div>
                 </div>
 
-                <div className="content_base content_info">
-                    <h3 style={{ color: "#f0564a", fontWeight: "bold" }}>YÊU CẦU THUÊ</h3>
+                {/* Phần dưới: Danh sách yêu cầu */}
+                <div className="player-bottom-section">
+                    <h3 className="player-title">YÊU CẦU THUÊ</h3>
                     <PlayerBookingList />
-
                 </div>
-
             </div>
-
         </div>
     );
-}
+};
 
 export default PlayerEdit;
