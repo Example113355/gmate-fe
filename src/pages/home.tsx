6b82e44b707e141df4c9818b1b4a2baae7b59610
Hungww <luuchanhung123@gmail.com>
2024-11-24 09:28:24 +0700

    fix service worker

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -4,7 +4,7 @@ const HomePage = () => {
   openNotification('success', 'Welcome', 'You are on the home page')
   return (
     <div className="h-full w-full">
-      <h1 className="text-2xl text-center mt-20">Home Page</h1>
+      <h1 className="text-2xl text-center mt-20">Home Page 24/11 9:27</h1>
     </div>
   )
 }
