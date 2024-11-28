4200ccc6600a14cab73a63820364d88db933e65f
TranSon <tson113355@gmail.com>
2024-11-29 06:03:43 +0700

    feat: integrate api call for authen

---
+++ src/contexts/UserContext.tsx
@@ -0,0 +1,66 @@
+import React, { createContext, useState, Dispatch, SetStateAction } from "react";
+
+interface User {
+    _id: string;
+    firstName: string;
+    lastName: string;
+    gender: string;
+    dateOfBirth: Date;
+    isGmater: boolean;
+    avatar: string;
+    email: string;
+    password: string;
+    isProfileCompleted: boolean;
+    createdAt: Date;
+    updatedAt: Date;
+    __v: number;
+}
+
+interface UserContextType {
+    user: User;
+    setUser: (user: User) => void;
+}
+
+const defaultUser: User = {
+    _id: '',
+    firstName: '',
+    lastName: '',
+    gender: '',
+    dateOfBirth: new Date(),
+    isGmater: false,
+    avatar: '',
+    email: '',
+    password: '',
+    isProfileCompleted: false,
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    __v: 0,
+}
+
+const UserContextType = {
+    user: defaultUser,
+    setUser: (user: User) => {}
+}
+
+export const UserContext = createContext<UserContextType>({
+    user: defaultUser,
+    setUser: () => {}
+});
+
+export const UserProvider = ({ children }: { children: React.ReactNode }) => {
+    const [user, setUser] = useState<User>(defaultUser);
+
+    return (
+        <UserContext.Provider value={{user, setUser}}>
+            {children}
+        </UserContext.Provider>
+    );
+}
+
+export const useUser = () => {
+    const user = React.useContext(UserContext);
+    if (!user) {
+        throw new Error('useUser must be used within a UserProvider');
+    }
+    return user;
+}
--- src/pages/Login/LoginForm/LoginForm.tsx
+++ src/pages/Login/LoginForm/LoginForm.tsx
@@ -1,15 +1,67 @@
 import { Button, Form, Input } from 'antd';
 import { useState } from 'react';
 import openNotification from '../../../components/notify';
+import { post } from '../../../utils/http_2';
+import { useUser } from '../../../contexts/UserContext';
+
+interface LoginResponse {
+    status: number;
+    data: ResponseData;
+}
+
+interface ResponseData {
+    data: LoginData;
+}
+
+interface LoginData {
+    accessToken: string;
+    user: User;
+}
+
+interface User {
+    _id: string;
+    firstName: string;
+    lastName: string;
+    gender: string;
+    dateOfBirth: Date;
+    isGmater: boolean;
+    avatar: string;
+    email: string;
+    password: string;
+    isProfileCompleted: boolean;
+    createdAt: Date;
+    updatedAt: Date;
+    __v: number;
+}
+
 
 const LogInForm = () => {
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
+    const [loading, setLoading] = useState(false)
     const [form] = Form.useForm()
+    const { user, setUser } = useUser()
 
     function handleSubmit() {
         form.validateFields().then(() => {
-            openNotification('success', 'Success', 'You have successfully logged in!')
+            post('auth/login', {
+                email: email,
+                password: password
+            }).then((response) => {
+                const loginResponse = response as LoginResponse;
+                if (loginResponse.status === 200) {
+                    localStorage.setItem('accessToken', loginResponse.data.data.accessToken)
+                    console.log(user)
+                    console.log(loginResponse.data.data.user)
+                    const newUser = loginResponse.data.data.user as User
+                    setUser(newUser)
+                    console.log(user)
+                    openNotification('success', 'Success', 'Logged in successfully!')
+                }
+            }).catch(() => {
+                setLoading(false)
+                openNotification('error', 'Error', 'Invalid credentials!')
+            })
         }).catch(() => {
             openNotification('error', 'Error', 'Please fill in all the fields!')
         })
@@ -41,7 +93,7 @@ const LogInForm = () => {
 
             <Form.Item className="submitBtn">
                 <Button type="primary" htmlType="submit" onClick={handleSubmit}>
-                    Log In
+                    {loading ? 'Loading...' : 'Log in'}
                 </Button>
             </Form.Item>
         </Form>
